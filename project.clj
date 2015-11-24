519a0d2768ccd0fdce03d9183f7a94ae16771b00
Kazuma SATO <ranceworks@gmail.com>
2015-11-24 20:37:30 +0900

    #1 load config.yml

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clj-yaml "0.4.0"]]
   :eval-in-leiningen true)
--- src/leiningen/kalar.clj
+++ src/leiningen/kalar.clj
@@ -1,6 +1,12 @@
-(ns leiningen.kalar)
+(ns leiningen.kalar
+  ;  (:import (java.io File))
+  [:use [clojure.java.io :only [file]]]
+  [:require [clj-yaml.core :as yaml]])
 
 (defn kalar
   "I don't do a lot."
   [project & args]
-  (println  (:resource-paths project)))
+  (let [possible-config-dest (map #(file % "config.yml") (:resource-paths project))
+        config (first (filter #(.exists %) possible-config-dest))]
+    (println (yaml/parse-string (slurp (.getAbsolutePath config))))))
+
