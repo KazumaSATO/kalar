7c64ced8151e014624732dd93227e3e89fbbc940
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-01-31 17:21:38 +0900

    v0.1.8

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lein-tamaki "0.1.1-SNAPSHOT"
+(defproject lein-tamaki "0.1.1"
   :description "a leiningen plugin"
   :url "https://github.com/satokazuma/lein-tamaki"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [
                  [hiccup "1.0.5"]
-                 [tamaki "0.1.8-SNAPSHOT"]
+                 [tamaki "0.1.8"]
                  [me.raynes/fs "1.4.6"]
                  [leinjacker "0.4.1"]]
   :eval-in-leiningen true)
