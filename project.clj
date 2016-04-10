5cc9a74963dc3d11f17ebafa803691f79e2de64c
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-04-10 19:46:35 +0900

    update tamaki

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lein-tamaki "0.1.2.3"
+(defproject lein-tamaki "0.1.2.4"
   :description "a leiningen plugin"
   :url "https://github.com/satokazuma/lein-tamaki"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [hiccup "1.0.5"]
-                 [tamaki "0.2.1"]
+                 [tamaki "0.2.1.1"]
                  [enlive "1.1.6"]
                  [me.raynes/fs "1.4.6"]
                  [markdown-clj "0.9.86"]
