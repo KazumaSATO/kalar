81f6b502f1832a765c2f7e02603c4b05faab67df
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2016-12-17 21:57:37 +0900

    mod command

--- src/leiningen/tamaki.clj
+++ src/leiningen/tamaki.clj
@@ -1,19 +1,11 @@
 (ns leiningen.tamaki
-  [:require [clojure.java.io :as io]
-            [clojure.edn :as edn]
-            [leiningen.core.eval :as leval]
-            [me.raynes.fs :as fs]])
-
-
+  [:require [tamaki-core.invoke :as invk]
+            [leiningen.core.eval :as leval]])
 
 (defn tamaki
   "Automatically write all the project's code."
   ;{:subtasks [#'clean]}
   [project & args]
-  (println (leval/eval-in-project project `(tamaki-core.invoke/invoke ~@args) '(require 'tamaki-core.invoke)))
-  (case (first args)
-    ;    "clean" (leval/eval-in-project project '(tamaki-core.file/clean-dest) '(require 'tamaki-core.file))
-    ;"compile" (leval/eval-in-project project '(tamaki-core.server/tcompile) '(require 'tamaki-core.server))
-    ;"similarity" (leval/eval-in-project project '(tamaki.post.post/report-post-similarity) '(require 'tamaki.post.post))
-    ; nil            :not-implemented-yet
+  (if (invk/defined-step? (first args))
+    (leval/eval-in-project project `(tamaki-core.invoke/proc ~@args) '(require 'tamaki-core.invoke))
     (leiningen.core.main/warn "Unknown task.")))
