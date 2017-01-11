bc201a5e7fa650ac682c806552d48612103eae49
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-01-11 09:26:14 +0900

    clojars

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [hiccup "1.0.5"]
-                 [tamaki "0.3.0.0-SNAPSHOT"]
+                 [tamaki "0.3.0.0"]
                  [enlive "1.1.6"]
                  [me.raynes/fs "1.4.6"]
                  [markdown-clj "0.9.86"]
