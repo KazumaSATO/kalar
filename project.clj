f76e6d05f65139938091dc464d8b14a166507c35
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-01-17 09:28:23 +0900

    v0.2.0.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lein-tamaki "0.2.0.0"
+(defproject lein-tamaki "0.2.0.1"
   :description "a leiningen plugin"
   :url "https://github.com/satokazuma/lein-tamaki"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [hiccup "1.0.5"]
-                 [tamaki "0.3.0.0"]
+                 [tamaki "0.3.0.1"]
                  [enlive "1.1.6"]
                  [me.raynes/fs "1.4.6"]
                  [markdown-clj "0.9.86"]
